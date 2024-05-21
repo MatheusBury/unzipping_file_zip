86e5d46382c1c7d559e2ea809328c783e72d1c2d
Matheus Bury <106456353+MatheusBury@users.noreply.github.com>
2024-05-21 13:51:23 -0300

    Erro
    Coloquei o try: expection pro programa nao parar se der erro

--- unzipping.py
+++ unzipping.py
@@ -2,9 +2,9 @@
 import os
 import shutil
 
-path_name = r'G:\Drives compartilhados\Corrosion Segmentation Data\project_modec\MV30\EQUIP_AI_DELIVERY_05172024\alt_predictions'
+path_name = r'G:\Drives compartilhados\Corrosion Segmentation Data\project_modec\MV30\EQUIP_AI_DELIVERY_05172024\3d_mask'
 file_name = os.listdir(path_name)
-Output = r'C:\Users\Operacao\Pictures\MV30 MASK'
+Output = r'C:\Users\Operacao\Pictures\MV30 3D MASK'
 
 
 def descompactar(arquivo, pasta_destino):
@@ -12,10 +12,13 @@ def descompactar(arquivo, pasta_destino):
     arquivo_completo = os.path.join(path_name, arquivo)
 
     if arquivo.endswith('.zip'):
-   
-        shutil.unpack_archive(arquivo_completo, pasta_destino)
-        print(f'Arquivo {arquivo} descompactado com sucesso!')
-
+        try:
+            shutil.unpack_archive(arquivo_completo, pasta_destino)
+            print(f'Arquivo {arquivo} descompactado com sucesso!')
+        except Exception as e:
+            print(f'Erro ao descompactar o arquivo {arquivo}: {e}')
+            pass  # Continue with the next file
+        
 for arquivo in os.listdir(path_name):
     
     if os.path.isfile(os.path.join(path_name, arquivo)):
